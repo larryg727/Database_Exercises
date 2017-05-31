a47cd00813ebedeba105c04e5fa959eefd59efcc
larryg727 <larrygonzales727@gmail.com>
2017-05-31 14:54:01 -0500

    completed select exercise

--- albums_migration.sql
+++ albums_migration.sql
@@ -5,7 +5,7 @@ drop table IF EXISTS album;
 CREATE TABLE album(
   id INT UNSIGNED auto_increment primary key,
   artist VARCHAR(100),
-  name varchar(100),
+  name VARCHAR(100),
   release_date YEAR,
   sales FLOAT,
   genre VARCHAR(100)
---
+++ select_exercise.sql
@@ -0,0 +1,21 @@
+USE codeup_test_db;
+
+
+select 'Albums by Pink Floyd' as 'Description'\G
+SELECT name from album where artist = 'Pink Floyd';
+
+SELECT 'Year Sgt Pepper''s Lonely Hearts Club Band was released' as 'Description'\G
+Select name, release_date from album where name = 'Sgt. Pepper''s Lonely Hearts Club Band';
+
+select 'Genre for Nevermind' as 'Description'\G
+select name, genre from album where name = 'Nevermind';
+
+select 'Albums released in the 1990''s' as 'Description'\G
+select artist, name, release_date from album where release_date between '1990' and '1999';
+
+select 'Albums that had less than 20 million certified sales' as 'Description'\G
+select artist, name, sales from album where sales < '20';
+
+select 'Albums in the rock genre' as 'Desctiption'\G
+select artist, name, genre from album where genre = 'rock';
+

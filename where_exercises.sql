dd04a307787107b72ced94785bb3eac556d5aae2
larryg727 <larrygonzales727@gmail.com>
2017-06-01 15:31:27 -0500

    -completed where continued exercise

--- where_exercises.sql
+++ where_exercises.sql
@@ -2,20 +2,32 @@ SELECT
   first_name,
   last_name
 FROM employees
-WHERE first_name IN ('Irena', 'Vidya', 'Maya');
+WHERE (first_name = 'Irena'
+OR first_name = 'Vidya'
+OR first_name = 'Maya')
+AND gender = 'M';
 
 SELECT
   first_name,
   last_name
 FROM employees
-WHERE last_name LIKE 'E%';
+WHERE last_name LIKE 'E%'
+OR last_name LIKE '%e';
+
+SELECT
+  first_name,
+  last_name
+FROM employees
+WHERE last_name LIKE 'E%'
+      AND last_name LIKE '%e';
 
 SELECT
   first_name,
   last_name,
   hire_date
 FROM employees
-WHERE hire_date LIKE '199%';
+WHERE hire_date LIKE '199%'
+AND birth_date LIKE '%-12-25';
 
 SELECT
   first_name,
@@ -28,4 +40,5 @@ SELECT
   first_name,
   last_name
 FROM employees
-WHERE last_name LIKE '%q%';
+WHERE last_name LIKE '%q%'
+AND last_name NOT LIKE '%qu%';

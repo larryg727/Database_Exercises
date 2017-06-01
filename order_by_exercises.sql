efd4430be3a33076287c0c52978885bb54dd693f
larryg727 <larrygonzales727@gmail.com>
2017-06-01 16:24:11 -0500

    -order by step 2 complete

---
+++ order_by_exercises.sql
@@ -0,0 +1,44 @@
+SELECT
+  first_name,
+  last_name
+FROM employees
+WHERE (first_name = 'Irena'
+       OR first_name = 'Vidya'
+       OR first_name = 'Maya')
+ORDER BY first_name;
+
+SELECT
+  first_name,
+  last_name
+FROM employees
+WHERE last_name LIKE 'E%'
+      OR last_name LIKE '%e';
+
+SELECT
+  first_name,
+  last_name
+FROM employees
+WHERE last_name LIKE 'E%'
+      AND last_name LIKE '%e';
+
+SELECT
+  first_name,
+  last_name,
+  hire_date
+FROM employees
+WHERE hire_date LIKE '199%'
+      AND birth_date LIKE '%-12-25';
+
+SELECT
+  first_name,
+  last_name,
+  birth_date
+FROM employees
+WHERE birth_date LIKE '%-12-25';
+
+SELECT
+  first_name,
+  last_name
+FROM employees
+WHERE last_name LIKE '%q%'
+      AND last_name NOT LIKE '%qu%';

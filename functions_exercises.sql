ec71e33d2ec2ba603fe51ecd6f611d3867473249
larryg727 <larrygonzales727@gmail.com>
2017-06-02 11:37:28 -0500

    -Step 1 and 2 complete on functions exercise

---
+++ functions_exercises.sql
@@ -0,0 +1,48 @@
+SELECT count(*),
+  gender
+FROM employees
+WHERE (first_name = 'Irena'
+       OR first_name = 'Vidya'
+       OR first_name = 'Maya')
+GROUP BY gender;
+
+SELECT concat(
+  first_name,
+    ' ',
+  last_name)
+FROM employees
+WHERE last_name LIKE 'E%'
+      AND last_name LIKE '%e';
+
+SELECT
+  first_name,
+  last_name,
+  emp_no
+FROM employees
+WHERE last_name LIKE 'E%'
+      AND last_name LIKE '%e'
+ORDER BY emp_no DESC ;
+
+SELECT
+  first_name,
+  last_name,
+  hire_date,
+  birth_date
+FROM employees
+WHERE hire_date LIKE '199%'
+      AND birth_date LIKE '%-12-25'
+ORDER BY birth_date ASC, hire_date DESC;
+
+SELECT
+  first_name,
+  last_name,
+  birth_date
+FROM employees
+WHERE birth_date LIKE '%-12-25';
+
+SELECT
+  first_name,
+  last_name
+FROM employees
+WHERE last_name LIKE '%q%'
+      AND last_name NOT LIKE '%qu%';

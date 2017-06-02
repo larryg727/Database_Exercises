720f8b1a4bcb7e2605601d6f058ca502acfbdf55
larryg727 <larrygonzales727@gmail.com>
2017-06-02 10:34:41 -0500

    -completed step 3 of limit exercise

--- limit_exercises.sql
+++ limit_exercises.sql
@@ -2,3 +2,14 @@ SELECT last_name
 FROM employees
   GROUP BY last_name DESC
 LIMIT 10;
+
+SELECT
+  first_name,
+  last_name,
+  hire_date,
+  birth_date
+FROM employees
+WHERE hire_date LIKE '199%'
+      AND birth_date LIKE '%-12-25'
+ORDER BY birth_date ASC, hire_date DESC
+LIMIT 5;

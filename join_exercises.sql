a722564f26c5ba198e0976e6758a351b48b4e335
larryg727 <larrygonzales727@gmail.com>
2017-06-04 21:37:16 -0500

    -completed step 4 of join exercises

--- join_exercises.sql
+++ join_exercises.sql
@@ -25,4 +25,17 @@ where d.dept_name = 'Customer Service'
   AND t.to_date > curdate()
 GROUP BY t.title;
 
+SELECT
+  d.dept_name as 'Department Name',
+  CONCAT(e.first_name, ' ', e.last_name) as 'Name',
+  s.salary as 'Salary'
+  FROM employees as e
+JOIN dept_emp as de on e.emp_no = de.emp_no
+JOIN departments as d on de.dept_no = d.dept_no
+join salaries as s on e.emp_no = s.emp_no
+join dept_manager as dm on e.emp_no = dm.emp_no
+where dm.to_date > curdate()
+AND s.to_date > curdate()
+ORDER BY d.dept_name;
+
 
